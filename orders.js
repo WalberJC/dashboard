5c15a6b59d29a04a35d19fa795cde95a094fd0c4
Walber <walberg.s48@gmail.com>
2023-05-01 19:23:01 -0300

    :hammer: add scripts

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '87124',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'TV Samsung 60"',
+    productNumber: '02324',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'Iphone 14 Pro Max',
+    productNumber: '90923',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Books Julio Verne',
+    productNumber: '00128',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'Barcelona Home Shirt 22/23',
+    productNumber: '98464',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'MacBook Pro 16,2 pol',
+    productNumber: '00013',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '4322',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  
+]
